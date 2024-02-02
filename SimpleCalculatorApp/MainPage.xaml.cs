71dfe1c54c89235d95b550cd6d165e7d6031b601
M-AboWarda <mohammad@abowarda.com>
2024-02-02 16:08:12 +0100

    Final touches!

--- SimpleCalculatorApp/MainPage.xaml.cs
+++ SimpleCalculatorApp/MainPage.xaml.cs
@@ -32,6 +32,7 @@ namespace SimpleCalculatorApp
             // cleanup 
             Int64 parsed;
             if (!Int64.TryParse(ResultsBlock.Text, out parsed)) { ResultsBlock.Text = "0"; }
+            //if((sender as Button).DataContext == "pressed")
             // 
 
             String input = (sender as Button).Content.ToString();
@@ -42,12 +43,13 @@ namespace SimpleCalculatorApp
                 OperationHandler(input); 
 
             UpdateFontSizeToFit(OperationBlock, OperationBlockContainer.ActualWidth * 4 / 5);
+            CalculateBtn.DataContext = (input == "=")? "pressed" : ""; 
         }
         private void NumberHandler(string digitText)
         {
-            if((OperationBlock.Text.Length > 15 && OperationBlock.Text != "") || (ResultsBlock.Text.Length > 15 && OperationBlock.Text == "")) { return; }// limit
+            if((OperationBlock.Text.Length > 12 && OperationBlock.Text != "") || (ResultsBlock.Text.Length > 15 && OperationBlock.Text == "" && CalculateBtn.DataContext.ToString() != "pressed")) { return; }// limit
             if(OperationBlock.Text == "") { 
-                ResultsBlock.Text = (ResultsBlock.Text == "0")? digitText :  ResultsBlock.Text + digitText;
+                ResultsBlock.Text = (ResultsBlock.Text == "0" || CalculateBtn.DataContext.ToString() == "pressed")? digitText :  ResultsBlock.Text + digitText;
                 UpdateFontSizeToFit(ResultsBlock, ResultsContainer.ActualWidth - 20);// 20 Margin
             }
             else
@@ -60,49 +62,74 @@ namespace SimpleCalculatorApp
                 OperationBlock.Text = (operation != "=")? operation : OperationBlock.Text;
             }else {
                 Calculate(ResultsBlock.Text, OperationBlock.Text.Substring(1, OperationBlock.Text.Length - 1), OperationBlock.Text.Substring(0, 1));
-                OperationBlock.Text = (operation != "=")? operation : "";}
+                OperationBlock.Text = (operation != "=")? operation : "";
+            }
         }
         private void Calculate(string a, string b, string operation)
         {
+            bool success = false;
             switch (operation)
             {
                 case "-":
-                    TrySubtract(a, b);
+                    success = TrySubtract(a, b);
                     break;
                 case "+":
-                    TryAdd(a, b);
+                    success = TryAdd(a, b);
                     break;
                 case "/":
-                    TryDivide(a, b);
+                    success = TryDivide(a, b);
                     break;
                 case "x":
-                    TryMultiply(a, b);
+                    success = TryMultiply(a, b);
                     break;
-            }
+            }// Interger OverFlow 9 223 372 036 854 775 807
+            if (!success) ResultsBlock.Text = (operation == "/") ? "Illegal Zero Division" : "Interger OverFlow";
             UpdateFontSizeToFit(ResultsBlock, ResultsContainer.ActualWidth - 20);// 20 Margin
         }
-        private bool TrySubtract(string a, string b) { ResultsBlock.Text = (Int64.Parse(a) - Int64.Parse(b)).ToString(); return true; }
-        private bool TryAdd(string a, string b) { ResultsBlock.Text = (Int64.Parse(a) + Int64.Parse(b)).ToString(); return true; }
-        private bool TryDivide(string a, string b) { ResultsBlock.Text = (Int64.Parse(a) / Int64.Parse(b)).ToString(); return true; }
-        private bool TryMultiply(string a, string b) { ResultsBlock.Text = (Int64.Parse(a) * Int64.Parse(b)).ToString(); return true; }
+        private bool TrySubtract(string a, string b) {
+            // overflow detection
+            if (Add(Int64.MinValue.ToString(), b) >= Int64.Parse(a)) return false; 
+            ResultsBlock.Text = Subtract(a,b).ToString();
+            return true;
+        }
+        private Int64 Subtract(string a, string b) { return (Int64.Parse(a) - Int64.Parse(b)); }
+        private bool TryAdd(string a, string b) {
+            // overflow detection
+            if (Subtract(Int64.MaxValue.ToString(), b) <= Int64.Parse(a)) return false;
+            ResultsBlock.Text = Add(a,b).ToString(); 
+            return true; 
+        }
+        private Int64 Add(string a, string b) { return (Int64.Parse(a) + Int64.Parse(b)); }
+        private bool TryDivide(string a, string b) { 
+            if (Int64.Parse(b) == 0) return false; // illegal zero division
+            ResultsBlock.Text = (Int64.Parse(a) / Int64.Parse(b)).ToString(); 
+            return true;
+        }
+        private bool TryMultiply(string a, string b) {
+            //( a * b == c ) <=> ( c / b == a ) overflow detection
+            if (TryDivide(Multiply(a,b).ToString(),b)) { if (ResultsBlock.Text.ToString() != a) return false; }
+            ResultsBlock.Text = Multiply(a,b).ToString();
+            return true; 
+        }
+        private Int64 Multiply(string a, string b) { return (Int64.Parse(a) * Int64.Parse(b)); }
 
+        private void ClearBtn_Clicked(object sender, RoutedEventArgs e)
+        {
+            TextBlock clean = (OperationBlock.Text != "") ? OperationBlock : ResultsBlock;
+            clean.Text = (clean == OperationBlock)? "" : "0";
+            CalculateBtn.DataContext = "";
+        }
         private void UpdateFontSizeToFit(TextBlock box, double containerWidth, int defaultSize = 40)
         {
             box.FontSize = defaultSize;
             box.Measure(new Size(double.PositiveInfinity, double.PositiveInfinity));
 
-            while(box.DesiredSize.Width > containerWidth && box.FontSize > 8)// minimum font size 8
+            while (box.DesiredSize.Width > containerWidth && box.FontSize > 8)// minimum font size 8
             {
                 box.FontSize = box.FontSize - 1;
                 box.Measure(new Size(double.PositiveInfinity, double.PositiveInfinity));
 
             }
-        }
-
-        private void ClearBtn_Clicked(object sender, RoutedEventArgs e)
-        {
-            TextBlock clean = (OperationBlock.Text != "") ? OperationBlock : ResultsBlock;
-            clean.Text = (clean == OperationBlock)? "" : "0";            
         }
     }
 }
